3a2e60d4e9d29a5356302328be62aaabd0a63c5b
Anna <anshalneva@gmail.com>
2026-02-25 21:21:14 +0100

    Added lesson task

---
+++ lessonFivePart2/index.js
@@ -0,0 +1,35 @@
+import http from "http"
+const PORT = 3333
+const server = http.createServer((req,res) => {
+   res.statusCode = 200
+  res.setHeader("Contant-Type","text/plain")
+
+if(req.method === "GET"){
+ if(req.url === "/"){
+    res.end("Main Page")
+  }
+else if(req.url === "/about"){
+     res.end("About us")
+  }
+else if(req.url === "/contacts"){
+  res.end("Contacts")
+} else{
+  res.statusCode = 400
+  res.end("Page not found")
+}
+}else if (req.method === "POST") {
+    if (req.url === "/submit") {
+      req.end("Form submitted!");
+    } else {
+      res.statusCode = 404;
+      res.end("Page not found");
+    }
+  } else {
+    res.statusCode = 405;
+    res.end("Method not allowed");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}`)
+})
