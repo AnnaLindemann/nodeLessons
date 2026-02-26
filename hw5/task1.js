07e30884166f0a20b2fd2e4790e1d7d131cdfae5
Anna <anshalneva@gmail.com>
2026-02-26 17:19:38 +0100

    Added solution for the task1

---
+++ hw5/task1.js
@@ -0,0 +1,18 @@
+import http from "http"
+
+const port = 3334
+const server = http.createServer((req, res) => {
+const header = req.headers["authorization"]
+if(typeof header === "string"){
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Status:200 - Authorization header received")
+} else{
+  res.statusCode = 401
+  res.end("Status:401 - Unauthorized")
+}
+})
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
