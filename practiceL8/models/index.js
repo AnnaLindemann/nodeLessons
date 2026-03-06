61cb0998b00b4a04ecbe670def4728f310030667
Anna <anshalneva@gmail.com>
2026-03-06 20:01:15 +0100

    Changed method of Category

--- practiceL8/models/index.js
+++ practiceL8/models/index.js
@@ -1,7 +1,7 @@
 import Product from "./Product";
 import Category from "./Category";
 
-Category.hasOne(Product, {foreignKey: "categoryName", as: "products"})
+Category.hasMany(Product, {foreignKey: "categoryName", as: "products"})
 Product.belongsTo(Category, {foreignKey: "categoryName", as: "products"})
 
 export {Product, Category}
