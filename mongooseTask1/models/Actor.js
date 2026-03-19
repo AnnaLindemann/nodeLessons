0c32083369a85f2ada674874c1b0e7756b1bb509
Anna <anshalneva@gmail.com>
2026-03-19 21:09:33 +0100

    Added models Movie and Actor

---
+++ mongooseTask1/models/Actor.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const actorSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true,
+    minlength: 2,
+    trim: true,
+  },
+  birthdate: {
+    type: Date,      
+    required: true,
+      },
+  movies:[
+     {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: "Movie"
+     }
+   ]
+
+})
+
+const Actor = mongoose.model("Actor", actorSchema)
+
+export default Actor
